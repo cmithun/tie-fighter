151720434ac63102301ea870e532f343027f8f6a
rjhedblom <ryan.hedblom@MINMTH-NMX3642.local>
2015-08-03 15:59:27 -0500

    DB owner switched in tie-fighter database

--- models/tie-fighter-database.js
+++ models/tie-fighter-database.js
@@ -3,5 +3,5 @@ var connectionString = process.env.DATABASE_URL || 'postgres://elximttdmryywf:Zc
 
 var client = new pg.Client(connectionString);
 client.connect();
-var query = client.query('CREATE DATABASE linkbase WITH OWNER nodedev');
+var query = client.query('CREATE DATABASE linkbase WITH OWNER elximttdmryywf');
 query.on('end', function() { client.end(); });
