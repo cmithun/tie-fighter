78be2310c93f5d10e35bb7d7856b6e42403ddac6
rjhedblom <ryan.hedblom@MINMTH-NMX3642.local>
2015-08-03 15:20:55 -0500

    Database updates

--- models/linkbasetable.js
+++ models/linkbasetable.js
@@ -5,3 +5,4 @@ var client = new pg.Client(connectionString);
 client.connect();
 var query = client.query('CREATE TABLE link_base (id SERIAL PRIMARY KEY, link1 text not null, link2 text not null,link3 text not null, link4 text not null,link5 text not null)');
 query.on('end', function() { client.end(); });
+
