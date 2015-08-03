ab896e84a6c70f319e0c2812432ff61ac4fbb1ac
rjhedblom <ryan.hedblom@MINMTH-NMX3642.local>
2015-08-03 14:33:31 -0500

    Replaced localhost with heroku address

--- models/linkbasetable.js
+++ models/linkbasetable.js
@@ -1,5 +1,5 @@
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://nodedev:nodedev@localhost:5432/linkbase';
+var connectionString = process.env.DATABASE_URL || 'postgres://elximttdmryywf:Zcg0gq6R2fBIbIy9cWvveuMkn-@ec2-174-129-26-115.compute-1.amazonaws.com:5432/d5pkqb2eupci9e';
 
 var client = new pg.Client(connectionString);
 client.connect();
